1b2d0f764c9199e6e38b42c2660e2dbab4a4e245
minhho3199 <cryaosis132@gmail.com>
2021-03-28 14:20:20 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// $(window).scroll(function() {
+//     if ($("body").scrollTop() >= 300) {
+//         $("#nav-wrap").css({
+//             "backgroundColor": "white"
+//         });
+//         $("#title").css("color", "black");
+//         $(".nav-link").removeClass("original-hover");
+//         $(".nav-link").addClass("changed-hover");
+//     }
+//     else {
+//         $("#nav-wrap").css("backgroundColor", "black");
+//         $("#title").css("color", "white");
+//         $(".nav-link").removeClass("changed-hover");
+//         $(".nav-link").addClass("original-hover");        
+//     }
+// });
+
+alert(eval("var x = 5, y = 7; x + y;"));
